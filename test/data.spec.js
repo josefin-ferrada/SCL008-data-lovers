9c3a260778c7ddea8dcd1cbe9baeb30d76a99318
LauraRamirez1 <laura.ramirez1@gmail.com>
2019-03-06 12:14:12 -0300

    test filter

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,16 +1,51 @@
 global.window = global;
 global.assert = require('chai').assert;
-require('../src/data/pokemon/pokemon.js');
+require('../src/data');
 require('./data.spec.js');
 
+describe('data', () => {
 
-// describe('example', () => {
+    it('debería ser un objeto', () => {
+        assert.equal(typeof data, 'object');
+    });
 
-//     it('debería ser una función', () => {
-//         assert.equal(typeof example, 'function');
-//     });
+    describe('data.sortData', () => {
 
-//     it('debería retornar "example"', () => {
-//         assert.equal(example(), 'example');
-//     });
-// })
+        it('deberia ser una función', () => {
+            assert.equal(typeof window.data.sortData, 'function');
+        });
+
+    });
+
+    describe('data.filterData', () => {
+
+        const pokemonData = {
+            "pokemon": [{
+                    "id": 1,
+                    "name": "Bulbasaur",
+                    "type": [
+                        "Grass",
+                        "Poison"
+                    ]
+                },
+                {
+                    "id": 4,
+                    "name": "Charmander",
+                    "type": [
+                        "Fire"
+                    ]
+                },
+            ],
+        }
+
+        it('deberia ser una función', () => {
+            assert.equal(typeof window.data.filterData, 'function');
+        });
+        it('debería retornar true ', () => {
+            assert.isArray(pokemonData.pokemon);
+        });
+        it('debería retornar arreglo con objetos con tipo Fire', () => {
+            assert.deepEqual(window.data.filterData(pokemonData.pokemon, 'Fire'), [{ id: 4, name: 'Charmander', type: ['Fire'] }]);
+        });
+    });
+});
